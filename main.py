47d5aaedc05052605ac78dd75ed3ec810e68bfb4
Connor Taffe <cpaynetaffe@gmail.com>
2014-09-09 12:36:11 -0500

    more error catching

--- main.py
+++ main.py
@@ -7,44 +7,40 @@ import urllib2
 def get_url(q, url):
 	try:
 		s = urllib2.urlopen(url).read()
-		q.put(s) # different line, doesn't lock
-	except (ValueError, urllib2.HTTPError) as e:
-		q.put(e) # put error
+		if type(s) is str: q.put(s)
+		else: q.put(Exception("urllib2 read() returned non-string"))
+	except (ValueError, urllib2.HTTPError) as e: q.put(e) # put error
 	
 # create que, cool for threading
 q = mp.Queue()
 
 # open file
-try:
-	f = open("ips.txt", "r")
-except:
-	print "Error: unable to open input file"
+try: f = open("ips.txt", "r")
+except: print "Error: unable to open input file"
+
+threads = []
 
 # create threads
-threads = []
 for line in f:
 	try:
 		t = mp.Process(target = get_url, args = (q, line))
 		t.start()
 		threads.append(t)
-	except:
-		print "Error: unable to start thread"
+	except: print "Error: unable to start thread"
 
 
 # close file
 f.close()
 
 # print urls
-try:
-	f = open("output.txt", "w")
+try: f = open("output.txt", "w")
 except:
-	print "Error: unable to open output file"
+	print "Error: unable to open output file" 
+	exit(1)
 
 for t in threads:
 	s = q.get()
-	if type(s) is str:
-		f.write(s)
-	else:
-		print "Error: %s" % s
+	if type(s) is str: f.write(s)
+	else: print "Error: %s" % s
 
 f.close()
